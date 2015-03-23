da2a42e29b96eacdf9f5c9fd964517be5baa99c8
Pavel Bucka <pavelbucka@icloud.com>
2015-03-23 21:53:34 +0100

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   
   has_many :user_skills
   has_many :skills, through: :user_skills
+  
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
