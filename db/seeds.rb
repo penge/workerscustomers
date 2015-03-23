e393705e419271e748dfb75f7e341cad832f265f
Pavel Bucka <pavelbucka@icloud.com>
2015-03-23 22:52:09 +0100

    Define seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Clean db
+
+UserSkill.delete_all
+Skill.delete_all
+User.delete_all
+
+
+# Create basic skills
+
+skills = [
+  'cook',
+  'clean house',
+  'fix computer',
+  'draw pictures',
+  'write lyrics',
+  'write stories',
+  'make pancakes',
+  'bake bread',
+  'train aikido',
+  'translate english',
+  'translate german',
+  'translate french',
+  'create website',
+  'repair car',
+  'fix kitchen',
+].each do |skill|
+  Skill.create!(name: skill)
+end
+
+def random_skills(skills, count)
+  Skill.where(name: skills.sample(count))
+end
+
+
+# Create users
+
+tom = User.create!(
+  name: 'Tom',
+  surname: 'Aubegader',
+  email: 'tom@abc.com',
+  password: 'tom',
+  skills: random_skills(skills, 3),
+)
+
+ema = User.create!(
+  name: 'Ema',
+  surname: 'Marden',
+  email: 'ema@abc.com',
+  password: 'ema',
+  skills: random_skills(skills, 3),
+)
+
+
+# Create batch of workers
+
+(1..200).each do |index|
+  User.create!(
+    name: "Worker#{index}",
+    surname: "Workeringon",
+    email: "worker#{index}@workeringon.com",
+    password: 'worker',
+    skills: random_skills(skills, 3),
+  )
+end
