f0a50d589199550ef79df613949a2bf68cc03e78
Pavel Bucka <pavelbucka@icloud.com>
2015-03-23 22:14:36 +0100

    Define authenticate_user! method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
   
   helper_method :current_user
 
+  protected
+  def authenticate_user!
+    return true if session[:user_id]
+    
+    flash[:notice] = 'You need to log in first.'
+    redirect_to login_path
+    return false
+  end
+
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
