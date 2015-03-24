1b099135e0a7ee1bb0fbcf76c09e80b4113cadad
Pavel Bucka <pavelbucka@icloud.com>
2015-03-24 10:38:30 +0100

    Define application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,54 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  $(document).on('input', '#search', function() {
+    $input = $(this);
+    value = $input.val();
+    if (!value) {
+      $('#results').empty();
+      return;
+    }
+    
+    $.get('skills', {query: value}).done(function(data) {
+      $('#results').show();
+    });
+  });
+  
+  $(document).on('click', '.submit-button', function(e) {
+    e.preventDefault();
+    $(this).closest('form').submit();
+  });
+  
+  $(document).on('click', '#results ul li', function(e) {
+    $item = $(this);
+    var id = $item.data('id');
+    var val = $item.text();
+    if ($.inArray(id, getPickIds()) == -1) {
+      $item.remove();
+      $('#picks').append('<span class="pick" data-id="' + id + '">' + val + '</span>')
+      $('#pick-actions').removeClass('hide');
+    }
+  });
+  
+  $(document).on('click', '.pick', function(e) {
+    $(this).remove();
+    if (!$('.pick').length) {
+      $('#pick-actions').addClass('hide');
+    }
+  });
+  
+  $(document).on('click', '#search-button', function(e) {
+    e.preventDefault();
+    $('#results').hide();
+    var ids = getPickIds();
+  });
+  
+  var getPickIds = function() {
+    return $.map($('.pick'), function(pick) {
+      return $(pick).data('id');
+    });
+  };
+});
