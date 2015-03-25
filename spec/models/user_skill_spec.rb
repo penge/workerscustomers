80918f75e3aadd6e1c5577c727b7a142d1635749
Pavel Bucka <pavelbucka@icloud.com>
2015-03-25 14:33:26 +0100

    Update user_skill_spec

--- spec/models/user_skill_spec.rb
+++ spec/models/user_skill_spec.rb
@@ -9,30 +9,45 @@ RSpec.describe UserSkill, type: :model do
   end
   
   describe '.user_ids_having_required_skills' do
-    it 'returns user ids having all the required skills' do
+    before :each do
+      @adam.skills.delete_all
+      @eva.skills.delete_all
+    end
+    
+    it 'returns empty array if no one has the required skill' do
       user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id])
       expect(user_ids).to be_empty
-      
+    end
+    
+    it 'returns empty array if no one has all the required skills' do
       @adam.skills << @make_origami
-      @eva.skills << @teach_english
-      
-      # Look for one skill
-      
-      user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id])
-      expect(user_ids).to eq [@adam.id]
-      
-      user_ids = UserSkill.user_ids_having_required_skills([@teach_english.id])
-      expect(user_ids).to eq [@eva.id]
-      
-      # Look for multiple skills
       
       user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id, @teach_english.id])
       expect(user_ids).to be_empty
+    end
+    
+    it 'returns array with one user id that have the required skill' do
+      @adam.skills << @make_origami
       
-      @adam.skills << @teach_english
+      user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id])
+      expect(user_ids).to eq [@adam.id]
+    end
+    
+    it 'returns array with one user id that have all the required skills' do
+      @adam.skills << @make_origami << @teach_english
+      @eva.skills << @make_origami
       
       user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id, @teach_english.id])
       expect(user_ids).to eq [@adam.id]
+    end
+    
+    it 'returns array with multiple user ids having all the required skills' do
+      @adam.skills << @make_origami << @teach_english
+      @eva.skills << @make_origami << @teach_english
+      
+      user_ids = UserSkill.user_ids_having_required_skills([@make_origami.id, @teach_english.id])
+      expect(user_ids).to include @adam.id
+      expect(user_ids).to include @eva.id
     end
   end
 end
