06bb6643996afd65c6f5ba47805dfcf6e4924b4a
Pavel Bucka <pavelbucka@icloud.com>
2015-03-25 19:29:36 +0100

    Fix User validation for registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   has_many :skills, through: :user_skills
   
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
-  validates :surname, presence: true
+  validates :name, presence: true, unless: :new_record?
+  validates :surname, presence: true, unless: :new_record?
   
   def self.authenticate(email, password)
     User.find_by(email: email).try(:authenticate, password)
