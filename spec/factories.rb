17b537c70746cbd5dbb242a653ddc7e096f8f839
Pavel Bucka <pavelbucka@icloud.com>
2015-03-25 13:57:47 +0100

    Define :adam and :eva factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,19 @@
 FactoryGirl.define do
   factory :user do
+    name 'Johnny'
+    surname 'Mnemonic'
+    email 'johnny@mnemonic.com'
+    password 'johnny'
+  end
+  
+  factory :adam, class: User do
+    name 'Adam'
+    surname 'Durak'
+    email 'adam@durak.com'
+    password 'adam'
+  end
+  
+  factory :eva, class: User do
     name 'Eva'
     surname 'Modra'
     email 'eva@modra.com'
