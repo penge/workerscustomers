3b4a89f158c579555f201758250f4cb5265dfefc
Pavel Bucka <pavelbucka@icloud.com>
2015-03-25 13:59:35 +0100

    Use helpers in skill_spec

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,29 +1,23 @@
 require 'rails_helper'
+require 'helpers'
 
 RSpec.describe Skill, type: :model do
+  include Helpers
+  
   before :all do
-    Skill.delete_all
-    create(:skill, name: 'draw pictures')
-    create(:skill, name: 'cut wood')
-    create(:skill, name: 'make origami')
-    create(:skill, name: 'make glass')
-    create(:skill, name: 'teach English')
-    create(:skill, name: 'teach Spanish')
-    create(:skill, name: 'learn English')
-    create(:skill, name: 'learn Spanish')
-    create(:skill, name: 'work with animals')
+    before_all
   end
   
   describe '.alike_skills' do
     it 'returns skills whose name contains substring' do
       search = Skill.alike_skills('cut', 30)
       expect(search.size).to eq 1
-      expect(search.first.name).to eq 'cut wood'
+      expect(search.first.name).to eq @cut_wood.name
       
       search = Skill.alike_skills('wo', 30)
       expect(search.size).to eq 2
-      expect(search.first.name).to eq 'cut wood'
-      expect(search.second.name).to eq 'work with animals'
+      expect(search.first.name).to eq @cut_wood.name
+      expect(search.second.name).to eq @work_with_animals.name
     end
 
     it 'returns limited results' do
